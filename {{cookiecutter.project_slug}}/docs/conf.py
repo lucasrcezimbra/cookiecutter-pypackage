75ee2480b96beb713613acd34a49d325a1c143b8
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-09-22 18:24:52 -0300

    Fix docs/conf

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -32,7 +32,7 @@ import {{ cookiecutter.project_slug }}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['myst-parser', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
